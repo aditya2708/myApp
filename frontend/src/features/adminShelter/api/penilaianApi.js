23a4bbda5022c67e05fbd2db8406fc62ba485497
aditya2708 <adityahadywijaya@gmail.com>
2025-09-15 11:52:17 +0700

    feat: add optional semester query for penilaian API

--- frontend/src/features/adminShelter/api/penilaianApi.js
+++ frontend/src/features/adminShelter/api/penilaianApi.js
@@ -1,86 +1,102 @@
 import api from '../../../api/axiosConfig';
-import { ADMIN_SHELTER_ENDPOINTS } from '../../../constants/endpoints';
+
+// Base endpoint for all penilaian related requests
+const PENILAIAN_BASE_URL = '/admin-shelter/penilaian';
 
 export const penilaianApi = {
   /**
    * Get all penilaian with filters
    */
   getAllPenilaian: async (params = {}) => {
-    return await api.get('/admin-shelter/penilaian', { params });
+    return await api.get(PENILAIAN_BASE_URL, { params });
   },
 
   /**
    * Get penilaian by anak and semester
    */
-  getByAnakSemester: async (idAnak, idSemester) => {
-    return await api.get(`/admin-shelter/penilaian/anak/${idAnak}/semester/${idSemester}`);
+  getByAnakSemester: async (idAnak, idSemester, params = {}) => {
+    return await api.get(
+      `${PENILAIAN_BASE_URL}/anak/${idAnak}/semester/${idSemester}`,
+      { params }
+    );
   },
 
   /**
    * Get penilaian detail
    */
-  getPenilaianDetail: async (id) => {
-    return await api.get(`/admin-shelter/penilaian/${id}`);
+  getPenilaianDetail: async (id, params = {}) => {
+    return await api.get(`${PENILAIAN_BASE_URL}/${id}`, { params });
   },
 
   /**
    * Create new penilaian
    */
-  createPenilaian: async (penilaianData) => {
-    return await api.post('/admin-shelter/penilaian', penilaianData);
+  createPenilaian: async (penilaianData, params = {}) => {
+    return await api.post(PENILAIAN_BASE_URL, penilaianData, { params });
   },
 
   /**
    * Update penilaian
    */
-  updatePenilaian: async (id, penilaianData) => {
-    return await api.put(`/admin-shelter/penilaian/${id}`, penilaianData);
+  updatePenilaian: async (id, penilaianData, params = {}) => {
+    return await api.put(`${PENILAIAN_BASE_URL}/${id}`, penilaianData, { params });
   },
 
   /**
    * Delete penilaian
    */
-  deletePenilaian: async (id) => {
-    return await api.delete(`/admin-shelter/penilaian/${id}`);
+  deletePenilaian: async (id, params = {}) => {
+    return await api.delete(`${PENILAIAN_BASE_URL}/${id}`, { params });
   },
 
   /**
    * Bulk create penilaian
    */
-  bulkCreatePenilaian: async (penilaianArray) => {
-    return await api.post('/admin-shelter/penilaian/bulk', { penilaian: penilaianArray });
+  bulkCreatePenilaian: async (penilaianArray, params = {}) => {
+    return await api.post(
+      `${PENILAIAN_BASE_URL}/bulk`,
+      { penilaian: penilaianArray },
+      { params }
+    );
   },
 
   /**
    * Calculate nilai akhir
    */
-  calculateNilaiAkhir: async (idAnak, idSemester, mataPelajaran) => {
-    return await api.post('/admin-shelter/penilaian/calculate-nilai-akhir', {
-      id_anak: idAnak,
-      id_semester: idSemester,
-      mata_pelajaran: mataPelajaran
-    });
+  calculateNilaiAkhir: async (idAnak, idSemester, mataPelajaran, params = {}) => {
+    return await api.post(
+      `${PENILAIAN_BASE_URL}/calculate-nilai-akhir`,
+      {
+        id_anak: idAnak,
+        id_semester: idSemester,
+        mata_pelajaran: mataPelajaran
+      },
+      { params }
+    );
   },
 
   /**
    * Get jenis penilaian list
    */
-  getJenisPenilaian: async () => {
-    return await api.get('/admin-shelter/jenis-penilaian');
+  getJenisPenilaian: async (params = {}) => {
+    return await api.get('/admin-shelter/jenis-penilaian', { params });
   },
 
   /**
    * Get penilaian statistics for anak
    */
-  getStatistics: async (idAnak, idSemester) => {
-    return await api.get(`/admin-shelter/penilaian/statistics/${idAnak}/${idSemester}`);
+  getStatistics: async (idAnak, idSemester, params = {}) => {
+    return await api.get(
+      `${PENILAIAN_BASE_URL}/statistics/${idAnak}/${idSemester}`,
+      { params }
+    );
   },
 
   /**
    * Export penilaian to Excel
    */
   exportToExcel: async (params = {}) => {
-    return await api.get('/admin-shelter/penilaian/export', {
+    return await api.get(`${PENILAIAN_BASE_URL}/export`, {
       params,
       responseType: 'blob'
     });
